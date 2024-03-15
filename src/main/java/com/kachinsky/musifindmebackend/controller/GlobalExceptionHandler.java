97b64d0c862630d7154c562368729ee1ffb7ff55
Feodor Kachinsky <fedor15zil@gmail.com>
2024-03-15 19:54:33 +0300

    fix global exception handler

--- src/main/java/com/kachinsky/musifindmebackend/controller/GlobalExceptionHandler.java
+++ src/main/java/com/kachinsky/musifindmebackend/controller/GlobalExceptionHandler.java
@@ -2,18 +2,37 @@ package com.kachinsky.musifindmebackend.controller;
 
 import com.kachinsky.musifindmebackend.dto.ErrorResponse;
 import com.kachinsky.musifindmebackend.exception.ResourceNotFoundException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleResourceNotFoundException(ResourceNotFoundException ex) {
+        log.error(ex.getMessage());
+
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadInput(ConstraintViolationException ex) {
+        log.error(ex.getMessage());
+
+        String violations = ex.getConstraintViolations().stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining(", "));
+
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), violations);
+    }
 }
--- src/main/java/com/kachinsky/musifindmebackend/dto/ErrorResponse.java
+++ src/main/java/com/kachinsky/musifindmebackend/dto/ErrorResponse.java
@@ -1,8 +1,10 @@
 package com.kachinsky.musifindmebackend.dto;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 
 @RequiredArgsConstructor
+@Value
 public class ErrorResponse {
     private final int statusCode;
     private final String message;
